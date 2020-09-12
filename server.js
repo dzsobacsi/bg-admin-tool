4e888324e1d3605b42caf42f1d6714c58907a8ed
Tamas Berki <tberki77@gmail.com>
2020-09-12 17:48:31 +0200

    bugfix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const port = process.env.PORT || 8080
 const app //= express()
 
-app.use(cors())
+//app.use(cors())
 app.use(favicon(__dirname + '/build/favicon.ico'))
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname))
