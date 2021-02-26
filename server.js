6dff42dccb15f186cd21936ad48dc11066e2bab8
dzsobacsi <tberki77@gmail.com>
2021-02-26 20:38:15 +0100

    server simplified

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-
 const app = require('./app')
-const http = require('http')
 const config = require('./utils/config')
 
-const server = http.createServer(app)
-
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
